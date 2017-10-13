536e4ebd4c9874fbce31ff274b23e91d380b82a0
Jackie Weiss <kie2729@gmail.com>
2017-10-12 19:08:55 -0500

    i think it is done

--- main.py
+++ main.py
@@ -25,21 +25,36 @@ class Post(db.Model):
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-
-    if request.method == 'POST':
-        title = request.form['title']
-        content = request.form['content']
-        new_post = Post(title, content)
-        db.session.add(new_post)
-        db.session.commit()
-
-    posts = Post.query.filter_by().all()
+    posts = Post.query.order_by(Post.post_id).all()
+    """
+    selected_id = Post.query.filter_by(post_id=id).first()
+    title = request.args.get("Post.title")
+    content = request.args.get('Post.content')
+    """
 
     return render_template('blogmain.html', title='Build-a-blog', posts = posts)
 
-@app.route('/add_post', methods=['post'])
+@app.route('/add_post', methods=['get','post'])
 def addpost():
+     
     return render_template('add-post.html', title='Create a new post')
+
+@app.route('/latest', methods=['post'])
+def latest():
+    title = request.form['title']
+    content = request.form['content']
+
+    new_post = Post(title, content)
+    db.session.add(new_post)
+    db.session.commit()
+    
+    return render_template('latest.html', title=title, content=content)
+
+@app.route('/single')
+def single():
+    id= request.args['id']
+    post = Post.query.filter_by(post_id = id).first()
+    return render_template('single.html', title=post.title, content=post.content)
 
 if __name__ == '__main__':
     app.run()
